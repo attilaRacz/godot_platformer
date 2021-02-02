cc132ff533e8a05f592b455dd76604548c7de93a
raczat <raczat@invitech.hu>
2021-02-02 10:54:41 +0100

    back to menu from levels fix

--- src/Objects/Checkpoint.gd
+++ src/Objects/Checkpoint.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+onready var animation_player = $AnimationPlayer
 
 func _on_Checkpoint_body_shape_entered(body_id, body, body_shape, area_shape):
+	animation_player.play("save_checkpoint")
 	if body.name == 'Player':
 		body.hit_checkpoint()
