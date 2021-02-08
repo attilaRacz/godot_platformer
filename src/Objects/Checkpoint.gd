13f1bfd935782abc05a178e5a06b6ed9ab709ce0
raczat <raczat@invitech.hu>
2021-02-08 14:16:28 +0100

    enemy collision box bugfix

--- src/Objects/Checkpoint.gd
+++ src/Objects/Checkpoint.gd
@@ -3,6 +3,6 @@ extends Area2D
 onready var animation_player = $AnimationPlayer
 
 func _on_Checkpoint_body_shape_entered(body_id, body, body_shape, area_shape):
-	animation_player.play("save_checkpoint")
 	if body.name == 'Player':
+		animation_player.play("save_checkpoint")
 		body.hit_checkpoint()
